523e3ecf4959b1d401cac8f436a41aea29c2de9c
Tom1st <101661806+lovasatt@users.noreply.github.com>
2026-01-22 20:45:04 +0100

    feat(admin): add manual CS2 MM rank selection to player details
    Allows admins to set player ranks via a dropdown in the player edit page. Supports ranks 0-18 to maintain compatibility with CS2 matchmaking plugins.

--- src/web/pages/admintasks/tools_editdetails_player.php
+++ src/web/pages/admintasks/tools_editdetails_player.php
@@ -76,6 +76,7 @@ For support and installation notes visit http://www.hlxcommunity.com
 	    new PropertyPage_Property("fullName", "Real Name", "text"),
 	    new PropertyPage_Property("email", "E-mail Address", "text"),
 	    new PropertyPage_Property("homepage", "Homepage URL", "text"),
+	    new PropertyPage_Property("mmrank", "MM Rank", "select", "0/;1/Silver I;2/Silver II;3/Silver III;4/Silver IV;5/Silver Elite;6/Silver Elite Master;7/Gold Nova I;8/Gold Nova II;9/Gold Nova III;10/Gold Nova Master;11/Master Guardian I;12/Master Guardian II;13/Master Guardian Elite;14/Distinguished Master Guardian;15/Legendary Eagle;16/Legendary Eagle Master;17/Supreme Master First Class;18/The Global Elite"),
 	    new PropertyPage_Property("flag", "Country Flag", "select",$flagselect),
 	    new PropertyPage_Property("skill", "Points", "text"),
 	    new PropertyPage_Property("kills", "Kills", "text"),
